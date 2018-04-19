c619de0abf16c5daf21a35aa7801593dbe44bc8b
root <root@ip-172-31-18-160.ec2.internal>
2018-04-19 04:55:09 +0000

    aws permissions

--- app/start.php
+++ app/start.php
@@ -1,6 +1,6 @@
 <?php
 
-use Aws/S3/S3Client;
+use Aws\S3\S3Client ;
 
 require 'vendor/autoload.php';
 
@@ -8,6 +8,8 @@ $config = require('config.php');
 
 //S3
 $s3 = S3Client::factory([
+  'version' => 'latest',
+  'region' => $config['s3']['region'],
   'key' => $config['s3']['key'],
   'secret' => $config['s3']['secret']
 ]);
