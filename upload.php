5cad2fd6d0a1ca0f53a2018e12163b2bb3ad772c
johnmurph27 <jomu8186@colorado.edu>
2018-04-21 17:10:11 -0600

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,28 +1,34 @@
 <?php
+
 use Aws\S3\Exception\S3Exception;
 
 require 'app/start.php';
 
 if(isset($_FILES['file'])){
   $file = $_FILES['file'];
+
   $name = $file['name'];
   $tmp_name = $file['tmp_name'];
 
   $extension = explode('.', $name);
+  var_dump($extension);
   $extension = strtolower(end($extension));
 
   $key = md5(uniqid());
   $tmp_file_name = "{$key}.{$extension}";
-  $tmp_file_path = "/files/{$tmp_file_name}";
-  print $tmp_file_name;
-  echo "<br></br>";
-  print($tmp_file_path);
-  echo "<br></br>";
-  if(move_uploaded_file($tmp_file_name, $tmp_file_path)){
-	echo "file transfered";
-  }else{
-  	echo "not tranfered";
-  }
+  $tmp_file_path = "files/{$tmp_file_name}";
+
+  move_uploaded_file($tmp_name, $tmp_file_path);
+
+  // try{
+  //   $s3->putObject([
+  //     'Bucket' => $config['s3']['bucket'],
+  //     'Key' => ""
+  //   ])
+  // } catch(S3Exception $e){
+  //     die("Error uploading file");
+  // }
+}
 
   // try {
   //
@@ -35,10 +41,8 @@ if(isset($_FILES['file'])){
   // } catch(S3Exception $e){
   //   die("ther was an error");
   // }
-}
-else{
-  echo "Not Set";
-}
+
+
  ?>
 
  <!DOCTYPE html>
@@ -48,7 +52,7 @@ else{
    <title>Upload</title>
  </head>
  <body>
-   <form action="upload.php" method="post" enctype="multipart/form-data">
+   <form action="" method="post" enctype="multipart/form-data">
      <input type="file" name="file">
      <input type="submit" name="" value="Upload">
    </form>
